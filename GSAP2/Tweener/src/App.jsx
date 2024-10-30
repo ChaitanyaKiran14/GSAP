3e87442df7a50056aed3215404f00df1d26b4231
ChaitanyaKiran14 <g.chaitanya1419@gmail.com>
2024-10-30 11:48:31 +0530

    test

--- GSAP2/Tweener/src/App.jsx
+++ GSAP2/Tweener/src/App.jsx
@@ -1,7 +1,26 @@
+import { useRef, useLayoutEffect } from 'react'
+import gsap from "gsap"
+
 
 import './App.css'
 
 function App() {
+  //we can use useRef hook to create reference for the root level element
+  //useLayoutEffect hook is similar to useEffect hook but it fires after all the DOM Manipulations are done
+  const comp = useRef(null)
+
+  useLayoutEffect( () => {
+    //the function that is passed into the useLayoutEffect will runs once again after the component mounts and then once again after it unmounts due to its empty dependency array
+    //a gsap context is created inside the useLayoutEffect to record all GSAP animations that are that are setup during the context executon this allows for easy cleanup later on. 
+    //GSAP Context takes in function and optional scope element here the scope is comp that we created. 
+    //by setting the scope this ensures that all the animations that we make will only be effecting the children of the component referred to by the comp
+    let ctx = gsap.context(() => {},comp)
+
+
+    //last line of our function will contain the return...when the function is about to be unmounted or cleaned up it reverts the animations setup by the GSAP Context 
+  } , [])
+
+
   
 
   return (
